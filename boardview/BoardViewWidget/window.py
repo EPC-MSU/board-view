a6ac40ba9efd6ca034e8215aa338e644c23c513c
mihalin <Kisaverybad1>
2020-04-21 00:59:58 +0300

    Add add_point method

--- boardview/BoardViewWidget/window.py
+++ boardview/BoardViewWidget/window.py
@@ -1,4 +1,10 @@
+from PyQt5.QtCore import QPointF
 from PyQtExtendedScene import ExtendedScene
 
+from .pin import GraphicsManualPinItem
+
+
 class BoardView(ExtendedScene):
-    pass
+    def add_point(self, point: QPointF, number: int):
+        item = GraphicsManualPinItem(point, number)
+        self.add_component(item)
