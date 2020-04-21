822c2febca346543b2c0d1687476196172b5f846
mihalin <stopa3.10.1996@mail.ru>
2020-04-21 18:54:29 +0300

    Add point_selected signal

--- boardview/BoardViewWidget/window.py
+++ boardview/BoardViewWidget/window.py
@@ -1,10 +1,26 @@
-from PyQt5.QtCore import QPointF
-from PyQtExtendedScene import ExtendedScene
+from PyQt5.QtCore import QPointF, pyqtSignal, pyqtSlot
+from QtGui import QPixmap
+from typing import Optional
+
+from PyQtExtendedScene import ExtendedScene, AbstractComponent
 
 from .pin import GraphicsManualPinItem
 
 
 class BoardView(ExtendedScene):
+
+    point_selected = pyqtSignal(int)
+
+    def __init__(self, background: Optional[QPixmap] = None, zoom_speed: float = 0.001, parent=None) -> None:
+        super(BoardView, self).__init__(background, zoom_speed, parent)
+
+        self.on_component_left_click.connect(self.__component_selected)
+
+    @pyqtSlot(AbstractComponent)
+    def __component_selected(self, component: AbstractComponent):
+        if isinstance(component, GraphicsManualPinItem):
+            self.point_selected.emit(component.number)
+
     def add_point(self, point: QPointF, number: int):
         item = GraphicsManualPinItem(point, number)
         self.add_component(item)
