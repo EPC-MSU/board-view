7d4d8a5eaa80209440ccb5c9ccbcea8d81b6bb78
mihalin <Kisaverybad1>
2020-04-21 17:39:27 +0300

    Add select_point method

--- boardview/BoardViewWidget/window.py
+++ boardview/BoardViewWidget/window.py
@@ -8,3 +8,13 @@ class BoardView(ExtendedScene):
     def add_point(self, point: QPointF, number: int):
         item = GraphicsManualPinItem(point, number)
         self.add_component(item)
+
+    def select_point(self, number: int):
+        for component in self._components:
+            if isinstance(component, GraphicsManualPinItem):
+                if component.number == number:
+                    self.remove_all_selections()
+                    component.select()
+                    return
+
+        raise ValueError("No such pin " + str(number))
