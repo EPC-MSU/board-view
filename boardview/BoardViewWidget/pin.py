5f55dff3fcfb4969ce75843d4eaaf507f5c09ff3
mihalin <stopa3.10.1996@mail.ru>
2020-04-21 19:23:48 +0300

    Remove drag function for pins

--- boardview/BoardViewWidget/pin.py
+++ boardview/BoardViewWidget/pin.py
@@ -25,7 +25,7 @@ class _PinItemInfo:
 
 class GraphicsManualPinItem(AbstractComponent):
     def __init__(self, pos: QPointF, number: int):
-        super().__init__()
+        super().__init__(draggable=False)
 
         self.setZValue(Z.NEW_MANUAL_ELEMENT_PIN)
         self.setPos(pos)
