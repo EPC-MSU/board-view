bb975c2e93502c176ce6451c07409ef24c8e0a47
timerke <timerke@mail.ru>
2024-01-23 13:23:03 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="boardview",
-      version="0.1.2",
+      version="0.1.3",
       description="Board view widget for EyePoint",
       url="https://github.com/EPC-MSU/board-view",
       author="EPC MSU",
