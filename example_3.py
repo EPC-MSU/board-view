0f79102c20c9c38ca56b87524fb61c27d058df8e
timerke <timerke@mail.ru>
2024-10-04 14:41:57 +0300

    Added radio buttons to example

--- example_3.py
+++ example_3.py
@@ -1,8 +1,10 @@
 import os
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog, QHBoxLayout, QRadioButton, QVBoxLayout
 from epcore.filemanager import load_board_from_ufiv
 from boardview.tools.epcorecreator import create_board_view_from_board
+from PyQtExtendedScene.scenemode import SceneMode
 
 
 class Dialog(QDialog):
@@ -16,9 +18,32 @@ class Dialog(QDialog):
         board = load_board_from_ufiv(os.path.join(board_dir, "elements.json"))
         self.board_view = create_board_view_from_board(board, os.path.join(board_dir, "svg"))
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.board_view)
-        self.setLayout(self.layout)
+        self.button_no_action = QRadioButton("Обычный режим")
+        self.button_no_action.setChecked(True)
+        self.button_no_action.toggled.connect(self._set_mode)
+        self.button_edit = QRadioButton("Редактирование")
+        self.button_edit.toggled.connect(self._set_mode)
+        self.button_edit_group = QRadioButton("Редактирование составного компонента")
+        self.button_edit_group.toggled.connect(self._set_mode)
+        h_layout = QHBoxLayout()
+        h_layout.addWidget(self.button_no_action)
+        h_layout.addWidget(self.button_edit)
+        h_layout.addWidget(self.button_edit_group)
+
+        layout = QVBoxLayout()
+        layout.addLayout(h_layout)
+        layout.addWidget(self.board_view)
+        self.setLayout(layout)
+
+    @pyqtSlot()
+    def _set_mode(self) -> None:
+        if self.sender() == self.button_edit:
+            mode = SceneMode.EDIT
+        elif self.sender() == self.button_edit_group:
+            mode = SceneMode.EDIT_GROUP
+        else:
+            mode = SceneMode.NO_ACTION
+        self.board_view.set_scene_mode(mode)
 
 
 if __name__ == "__main__":
