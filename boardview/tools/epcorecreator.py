7b4541ea9d1557782ca103dcc3b4280934a34137
timerke <timerke@mail.ru>
2024-11-28 18:17:09 +0300

    Separated function to create graphics element item

--- boardview/tools/epcorecreator.py
+++ boardview/tools/epcorecreator.py
@@ -15,30 +15,8 @@ def create_board_view_from_board(board: Board, svg_dir: Optional[str] = None) ->
     board_view = BoardView(board.image)
 
     for element in board.elements:
-        if element.bounding_zone:
-            x_coords = [x for x, _ in element.bounding_zone]
-            y_coords = [y for _, y in element.bounding_zone]
-            x_min, x_max = min(x_coords), max(x_coords)
-            y_min, y_max = min(y_coords), max(y_coords)
-        elif element.width is not None and element.height is not None and element.center:
-            x_min = element.center[0] - element.height / 2
-            x_max = x_min + element.height
-            y_min = element.center[1] - element.width / 2
-            y_max = y_min + element.width
-        else:
-            x_min, x_max = None, None
-            y_min, y_max = None, None
-
-        if x_min is not None:
-            element_item = ElementItem(QRectF(0, 0, x_max - x_min, y_max - y_min), element.name)
-            element_item.setPos(x_min, y_min)
-            pins = [QPointF(pin.x, pin.y) for pin in element.pins]
-            element_item.add_pins(pins)
-
-            svg_file = os.path.join(svg_dir, f"{element.name}.svg")
-            if os.path.exists(svg_file):
-                element_item.set_element_description(svg_file, element.rotation)
-
+        element_item = create_graphics_element_item_from_element(element, svg_dir)
+        if element_item:
             board_view.add_element_item(element_item)
 
     return board_view
@@ -56,3 +34,39 @@ def create_element_from_graphics_element_item(element_item: ElementItem) -> Elem
     element = Element(pins=pins, name=element_data["name"], bounding_zone=element_rect, width=element_rect[2],
                       height=element_rect[3], rotation=element_data["rotation"])
     return element
+
+
+def create_graphics_element_item_from_element(element: Element, svg_dir: Optional[str] = None) -> Optional[ElementItem]:
+    """
+    :param element: epcore board element;
+    :param svg_dir: path to the folder with svg images of famous elements.
+    :return: graphics element item.
+    """
+
+    if element.bounding_zone:
+        x_coords = [x for x, _ in element.bounding_zone]
+        y_coords = [y for _, y in element.bounding_zone]
+        x_min, x_max = min(x_coords), max(x_coords)
+        y_min, y_max = min(y_coords), max(y_coords)
+    elif element.width is not None and element.height is not None and element.center:
+        x_min = element.center[0] - element.height / 2
+        x_max = x_min + element.height
+        y_min = element.center[1] - element.width / 2
+        y_max = y_min + element.width
+    else:
+        x_min, x_max = None, None
+        y_min, y_max = None, None
+
+    if x_min is not None:
+        element_item = ElementItem(QRectF(0, 0, x_max - x_min, y_max - y_min), element.name)
+        element_item.setPos(x_min, y_min)
+        pins = [QPointF(pin.x, pin.y) for pin in element.pins]
+        element_item.add_pins(pins)
+
+        svg_file = os.path.join(svg_dir, f"{element.name}.svg")
+        if os.path.exists(svg_file):
+            element_item.set_element_description(svg_file, element.rotation)
+
+        return element_item
+
+    return None
