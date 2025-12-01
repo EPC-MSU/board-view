3977824a50556fa0b99f5bf9a9217a86d553a61e
timerke <timerke@mail.ru>
2025-12-01 16:02:53 +0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
-import sys
 from setuptools import find_packages, setup
 
-
-PY_VERSION = sys.version_info[:2]
-dependencies = []
-if PY_VERSION == (3, 6):
-    dependencies.extend([
-        "PyQt5>=5.8.2, <=5.15.0",
-        "PyQtExtendedScene @ git+https://github.com/EPC-MSU/PyQtExtendedScene.git@v1.0.14"
-    ])
-else:
-    dependencies.extend([
-        "PyQt5",
-        "PyQtExtendedScene @ git+https://github.com/EPC-MSU/PyQtExtendedScene.git@master"
-    ])
 
 setup(name="boardview",
       version="0.1.4",
@@ -24,7 +10,11 @@ setup(name="boardview",
       license="MIT",
       packages=find_packages(),
       python_requires=">=3.6",
-      install_requires=dependencies,
+      install_requires=[
+          'PyQt5>=5.8.2, <=5.15.0; python_version=="3.6"',
+          'PyQt5; python_version>"3.6"',
+          "PyQtExtendedScene @ git+https://github.com/EPC-MSU/PyQtExtendedScene.git@master"
+    ],
       classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
